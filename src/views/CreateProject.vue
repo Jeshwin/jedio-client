18c8cd7fa9047275a35cac5f74e54fd816f518cc
Jeshwin Prince <jeshwinjprince@gmail.com>
2021-07-12 21:03:57 -0700

    update markdown preview

--- src/views/CreateProject.vue
+++ src/views/CreateProject.vue
@@ -3,16 +3,16 @@
   <form enctype="multipart/form-data" action="http://192.168.1.89:3000/create/project" method="post" id="project">
     <div class="flex flex-row my-3 border-b border-blue-200">
       <label for="title">Title:</label>
-      <input type="text" name="title" id="title">
+      <input class="w-56 overflow-x-auto" type="text" name="title" id="title">
     </div>
     <div class="flex flex-row my-3 border-b border-blue-200">
       <label for="category">Category:</label>
-      <input type="text" name="category" id="category">
+      <input class="w-36 overflow-x-auto" type="text" name="category" id="category">
     </div>
     <div class="flex flex-row my-3 border-b border-blue-200">
       <label for="description">Description:</label>
       <textarea v-model="input" placeholder="## Enter description here..." name="description" id="description" @input="update"></textarea>
-      <div class="prose prose-sm md:prose lg:prose-lg xl:prose-xl 2xl:prose-2xl prose-blue w-2/5 h-96 mb-4 p-6 overflow-y-auto rounded-r-lg border-2 border-l-0 border-gray-300 font-montserrat" v-html="compiledMarkdown"></div>
+      <div class="prose prose-sm md:prose lg:prose-lg xl:prose-xl 2xl:prose-2xl prose-blue w-2/5 h-96 mb-4 p-6 overflow-y-auto rounded-r-lg border-2 border-l-0 border-purple-400 font-montserrat" v-html="compiledMarkdown"></div>
     </div>
     <div class="flex flex-row my-3 border-b border-blue-200">
       <label for="thumbnail">Thumbnail:</label>
@@ -64,7 +64,10 @@ label {
 input[type=text] {
   @apply px-2 rounded-md ring-2 ring-black focus:ring-blue-400;
 }
+input, textarea {
+  @apply focus:outline-none;
+}
 textarea[name=description] {
-  @apply w-2/5 h-96 resize-none text-base font-montserrat mb-4 p-6 rounded-l-lg border-2 border-black focus:border-yellow-300;
+  @apply w-2/5 h-96 resize-none text-base font-montserrat mb-4 p-6 rounded-l-lg border-2 border-black focus:border-gray-500;
 }
 </style>
