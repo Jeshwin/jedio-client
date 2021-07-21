85be827acf900f0c68ba938118316a1ba635de9a
Jeshwin Prince <jeshwinjprince@gmail.com>
2021-07-21 13:58:50 -0700

    minor tweaks

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div class="w-full m-auto pt-6 pb-10 bg-white shadow-inner">
+  <div class="w-full m-auto pt-6 pb-10 mt-16 bg-white shadow-inner">
     <div class="items-center text-purple-400 mx-auto text-center font-montserrat">
       <p class="text-lg">Jeshwin Prince 2021</p>
       <p class="text-sm">Made with Express, Vue, and Tailwind</p>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div class="grid grid-cols-1 h-full pt-40 pb-24 border-b border-black text-left">
+  <div class="grid grid-cols-1 h-full pt-32 pb-20 border-b border-black text-left">
     <div class="text-8xl w-full font-montserrat text-black">
       {{ intro }}
     </div>
