74750113f9e79121f0ad868912ea82df4c72d079
Jeshwin Prince <jeshwinjprince@gmail.com>
2021-07-21 16:32:35 -0700

    change link to creator

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -7,15 +7,15 @@
       <div class="text-7xl text-left w-full h-20 col-span-2 font-montserrat text-black">
         {{ project.title }}
       </div>
-      <div v-if="project.userId" class="flex flex-row justify-start  w-full self-start -mt-2">
+      <div v-if="project.userId" class="flex flex-row justify-start w-full self-start -mt-1">
         <img class="w-10 h-10 mr-2 rounded-full ring-1 ring-black" src="@/assets/default_pfp.png">
-        <a class="text-4xl text-gray-400 hover:text-purple-400 transition duration-300 font-quicksand font-semibold" :href="createrProfile" >By User #{{ project.userId }}</a>
+        <a class="text-4xl text-gray-400 hover:text-purple-800 transition duration-500 font-quicksand font-medium" :href="createrProfile" >@{{ username }}</a>
       </div>
-      <div v-else class="flex flex-row justify-start text-4xl w-full self-start -mt-2 font-quicksand text-gray-500 font-semibold">
+      <div v-else class="flex flex-row justify-start text-4xl w-full self-start -mt-1 font-quicksand text-gray-700 font-medium">
         <img class="w-10 h-10 mr-2 rounded-full ring-1 ring-black" src="@/assets/default_pfp.png">
         By Anon
       </div>
-      <div class="text-base w-full h-auto -mt-2 font-montserrat text-gray-500 text-right">
+      <div class="text-base w-full h-auto -mt-2 font-montserrat text-gray-700 text-right">
         {{ createdDate }}<br>{{ updatedDate }}
       </div>
     </div>
@@ -38,6 +38,7 @@ export default {
   data () {
     return {
       project: { description: '### Filler description\n > Filler' },
+      username: '',
       thumbnail: {},
       blobs: [],
       monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
@@ -50,6 +51,8 @@ export default {
     this.project = Projres.data[0]
     this.thumbnail = Thumbres.data[0]
     this.blobs = Blobres.data
+    const Userres = await axios.get(`http://localhost:3000/user/${this.project.userId}`).catch((err) => console.error(err))
+    this.username = Userres.data[0].username
   },
   computed: {
     source () {
