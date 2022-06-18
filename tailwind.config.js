fc91ec82d1cbc272e225e7104aff7ee2be6054ce
Jeshwin Prince <jeshwinjprince@gmail.com>
2022-06-17 17:26:00 -0700

    <style> add Jost and Open Sans fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-    fontFamily: {
-      montserrat: ["Montserrat"],
-      quicksand: ["Quicksand"],
-    },
-  },
-  plugins: [],
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+		fontFamily: {
+			montserrat: ["Montserrat"],
+			quicksand: ["Quicksand"],
+			jost: ["Jost"],
+			opensans: ["Open Sans"],
+		},
+	},
+	plugins: [],
 };
